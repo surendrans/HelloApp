80399cb0ae717d9ed5c10212051724043039fcdd
Sairam <sairam.s@spritle.com>
2015-04-25 11:31:22 +0530

    qrfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   before_create :gen_qrcode
   has_many :invitations
   def gen_qrcode
-    RQRCode::QRCode.new( "helloapph.herokuapp.com", :size => 4, :level => :h ).to_img_rgba([0,0,0,0],[0,0,0,255]).resize(500,500).save("https://helloapph.herokuapp.com/public/qr_codes/" + email + ".png")
+    RQRCode::QRCode.new( "helloapph.herokuapp.com", :size => 4, :level => :h ).to_img_rgba([0,0,0,0],[0,0,0,255]).resize(500,500).save(Rails.root.to_s + "/public/qr_codes/" + email + ".png")
   end
 end
