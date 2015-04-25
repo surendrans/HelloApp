03707a7ab9b9b4abcbb87c2a8ca81c816ef126fa
Sairam <sairam.s@spritle.com>
2015-04-25 05:33:56 +0530

    added status message on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         # format.json { render :show, status: "Success", location: @user }
-        format.json { render json: @user, status: :created}
+        format.json { render json: @user, status: :created, status_message: "success"}
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
